26fc184f05ab3b5974588d4b8c3abb6bc8ba17a4
Wandy Torres <wandytorres@gmail.com>
2026-04-21 20:11:03 +0000

    Project 3: Terraform S3 and CloudFront

---
+++ project-3-terraform-cloudfront/README.md
@@ -0,0 +1,20 @@
+# Project 3 - Terraform S3 + CloudFront
+
+## Objective
+Provision a static website in AWS using Terraform with S3 and CloudFront.
+
+## Resources created
+- S3 bucket
+- S3 website configuration
+- public bucket policy
+- index.html object
+- CloudFront distribution
+
+## Result
+Website available through CloudFront:
+https://d3l9nuu0paph7n.cloudfront.net
+
+## Commands used
+- terraform init
+- terraform plan
+- terraform apply
---
+++ project-3-terraform-cloudfront/main.tf
@@ -0,0 +1,82 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "website" {
+  bucket = "wandy-cloudfront-terraform-862087104485"
+}
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = "*",
+      Action = ["s3:GetObject"],
+      Resource = ["${aws_s3_bucket.website.arn}/*"]
+    }]
+  })
+}
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.website.id
+  key    = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "allow-all"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
