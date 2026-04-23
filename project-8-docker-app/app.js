37dd965e56e86a9e3a0a7586854e75319baa1bae
Wandy Torres <wandytorres@gmail.com>
2026-04-23 20:56:07 +0000

    Test ECS-only deployment

--- project-8-docker-app/app.js
+++ project-8-docker-app/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("🚀 ECS Version 2 - CI/CD Working!");
+  res.end("🚀 ECS Version 2 deployed from CI/CD Working!");
 });
 
 server.listen(3000, () => {
