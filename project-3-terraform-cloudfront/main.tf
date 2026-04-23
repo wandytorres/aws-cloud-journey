95a40070fe5c3e732239876ac8367306db73af08
Wandy Torres <wandytorres@gmail.com>
2026-04-23 15:23:11 +0000

    Format Terraform files

--- project-3-terraform-cloudfront/main.tf
+++ project-3-terraform-cloudfront/main.tf
@@ -29,18 +29,18 @@ resource "aws_s3_bucket_policy" "public_read" {
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow",
+      Effect    = "Allow",
       Principal = "*",
-      Action = ["s3:GetObject"],
-      Resource = ["${aws_s3_bucket.website.arn}/*"]
+      Action    = ["s3:GetObject"],
+      Resource  = ["${aws_s3_bucket.website.arn}/*"]
     }]
   })
 }
 
 resource "aws_s3_object" "index" {
-  bucket = aws_s3_bucket.website.id
-  key    = "index.html"
-  source = "index.html"
+  bucket       = aws_s3_bucket.website.id
+  key          = "index.html"
+  source       = "index.html"
   content_type = "text/html"
 }
 
